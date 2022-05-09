6e9bad5d2750f0d81c549fddf3a01bea14da1a71
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-08 21:50:34 -0300

    Resolucao

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,8 +1,8 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 /**
  * Controle de Acesso.
@@ -19,22 +19,59 @@ public class Principal {
     short log;
     short idade;
     do {
-      System.out.println("Entre com o número correspondente à opção desejada:\n"
-          + "1 - Acessar o estabelecimento\n" + "2 - Finalizar sistema e mostrar relatório: ");
+      imprime("Entre com o número correspondente à opção desejada:");
+      imprime("1 - Acessar o estabelecimento");
+      imprime("2 - Finalizar sistema e mostrar relatório");
+
       log = scanner.nextShort();
       if (log == 1) {
         System.out.println("Entre com a sua idade:");
         idade = scanner.nextShort();
         imprimeIdade(idade);
         pessoas.add(idade);
-        System.out.println("Entre com o número correspondente à opção desejada:\n"
-            + "1 - Acessar o estabelecimento\n" + "2 - Finalizar sistema e mostrar relatório");
-        log = scanner.nextShort();
+      } else if (log != 1 && log != 2) {
+        imprime("Entre com uma opção válida!");
       }
 
-    } while (log == 1);
-    imprime("RELATORIO");
+    } while (log != 2);
+    gerarRelatorio(pessoas);
     scanner.close();
+  }
+
+  static void gerarRelatorio(ArrayList<Short> pessoas) {
+    int totalPessoas = pessoas.size();
+    int menores = 0;
+    int adultos = 0;
+    int velhos = 0;
+
+    for (int i = 0; i < totalPessoas; i += 1) {
+      if (pessoas.get(i) < 18) {
+        menores += 1;
+      } else if (pessoas.get(i) >= 18 && pessoas.get(i) <= 49) {
+        adultos += 1;
+      } else {
+        velhos += 1;
+      }
+    }
+
+    imprime("----- Quantidade -----");
+    imprime("menores: " + menores);
+    imprime("adultas: " + adultos);
+    imprime("a partir de 50: " + velhos);
+    imprime("");
+    imprime("----- Percentual -----");
+    DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+    String menorPorcentagem = decimalFormat.format(menores * 100f / totalPessoas);
+    imprime("menores: " + menorPorcentagem + "%");
+
+    String adultoPorcentagem = decimalFormat.format(adultos * 100f / totalPessoas);
+    imprime("adultas: " + adultoPorcentagem + "%");
+
+    String velhoPorcentagem = decimalFormat.format(velhos * 100f / totalPessoas);
+    imprime("a partir de 50: " + velhoPorcentagem + "%");
+    imprime("");
+    imprime("TOTAL: " + (totalPessoas));
   }
 
   static void imprimeIdade(int idade) {
